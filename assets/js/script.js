a7f55cae86d1f4c97ce05146939a78a31db94e7c
Mar Martin Ortiz <167127217+dyhozza@users.noreply.github.com>
2024-06-12 00:31:22 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//declaracion de variables
+const precioBase = 400000;
+let cantidad = 0;
+//con querySelector seleccionamos el elemento que va a CAMBIAR del html
+//con el innerHTML le CAMBIO O AGREGO el valor a lo que seleccioné
+document.querySelector('#precio').innerHTML = precioBase;
+document.querySelector('#cantidad').innerHTML = cantidad;
